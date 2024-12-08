276516f016e8374d5033e10147735c890ac2e5e7
DimitrisP <108548087+DimitrisPoupos@users.noreply.github.com>
2024-12-08 17:07:33 +0200

    Revert "Adding items to cart"
    This reverts commit ed0955312c20e03c62b53e38edc74a2fc52d144d.

--- my-app/app/addToCart.tsx
+++ my-app/app/addToCart.tsx
@@ -1,17 +1,17 @@
-"use client";
+"use client"
 import { MenuItem } from "./page";
 
-type Props = { item: MenuItem };
 
-export default function AddToCart({ item }: Props) {
-  let cart: MenuItem[] = [];
-  function handleClick() {
-    cart.push(item);
-    console.log(cart);
-  }
-  return (
-    <button className=" hover:text-green-400 " onClick={handleClick}>
-      Add
-    </button>
-  );
+
+type Props = { item: MenuItem; };
+
+export default function AddToCart( {item} : Props) {
+    let cart: MenuItem[] = [];
+    function handleClick() {
+        cart.push(item);
+        console.log(cart);
+    }
+    return (
+        <button onClick={handleClick} >Add</button>
+    )
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,33 +1,27 @@
 import Image from "next/image";
 import AddToCart from "./addToCart";
-
-export type MenuItem = { price: number; title: string };
-
+export type MenuItem = { price: number; title: string;};
 let menu: MenuItem[] = [
-  { price: 2, title: "coffee" },
-  { price: 2, title: "toast" },
-  { price: 1, title: "water" },
+  { "price": 2, "title": "coffee" },
+  { "price": 2, "title": "toast" },
+  { "price": 1, "title": "water" }
 ];
 
+
+
 export default function Home() {
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="p-7 bg-neutral-700 rounded-md">
-        <ul className="space-y-4">
-          {menu.map((items) => {
-            return (
-              <>
-                <li
-                  key={items.title}
-                  className="flex gap-5 justify-center rounded-md p-2 border text-center w-52"
-                >
-                  {items.title} {items.price} <AddToCart item={items} />{" "}
-                </li>
-              </>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="flex items-center" >
+      <ul className="space-y-4">
+        {menu.map((items) =>{
+          return(
+            <>
+            <li className="p-2 border" >{items.title} {items.price} <AddToCart item = {items} /> </li>
+            </>
+          )
+        })}
+      </ul>
     </div>
   );
 }
